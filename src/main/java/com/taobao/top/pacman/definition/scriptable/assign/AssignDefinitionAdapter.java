3e1cf4f3b75a22420aae1ff28f17bad92fa33b03
houkun <houkun@taobao.com>
2014-07-08 17:28:37 +0800

    fix #1; assign null, add AssignTest

--- src/main/java/com/taobao/top/pacman/definition/scriptable/assign/AssignDefinitionAdapter.java
+++ src/main/java/com/taobao/top/pacman/definition/scriptable/assign/AssignDefinitionAdapter.java
@@ -6,30 +6,33 @@ import com.taobao.top.pacman.definition.scriptable.ScriptableUtil;
 
 public class AssignDefinitionAdapter extends DefinitionAdapter<AssignDefinition> {
 	private static final long serialVersionUID = 8350944160836998026L;
-
+	
 	public AssignDefinitionAdapter() {
 		this(null, null, null);
 	}
-
+	
 	public AssignDefinitionAdapter(Object definition, DefinitionAdapter<?> parent, String extensionName) {
 		super(definition, parent, extensionName);
 	}
-
+	
 	@Override
 	public DefinitionAdapter<?> jsFunction_End() {
 		return this.end();
 	}
-
+	
 	@Override
 	public void jsFunction_addActivity(Object activity) {
 		ScriptableUtil.throwNotSupportTheExtension(this, activity);
 	}
-
+	
 	public AssignDefinitionAdapter jsFunction_Value(Object value) {
-		this.setValue("Value", value);
+		if (value == null)
+			this.getDefinition().Value((Object) null);
+		else
+			this.setValue("Value", value);
 		return this;
 	}
-
+	
 	public AssignDefinitionAdapter jsFunction_To(Object value) {
 		this.setValue("To", value);
 		return this;
