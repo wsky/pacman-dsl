8a633a3d7342809157c1ce2e7596e9bbe31d4760
houkun <houkun@taobao.com>
2014-07-09 16:36:14 +0800

    #6; support pass ActivityWithResultDefintion to InArgumentDefintion

--- src/main/java/com/taobao/top/pacman/definition/InArgumentDefinition.java
+++ src/main/java/com/taobao/top/pacman/definition/InArgumentDefinition.java
@@ -6,29 +6,37 @@ public class InArgumentDefinition {
 	private Object constValue;
 	private VariableReferenceDefinition variable;
 	private InlinedFunctionDefinition function;
-
+	private ActivityWithResultDefinition activityWithResult;
+	
 	public InArgumentDefinition() {
 	}
-
+	
 	public InArgumentDefinition(Object constValue) {
 		this.constValue = constValue;
 	}
-
+	
 	public InArgumentDefinition(InlinedFunctionDefinition function) {
 		this.function = function;
 	}
-
+	
 	public InArgumentDefinition(VariableReferenceDefinition variable) {
 		this.variable = variable;
 	}
-
+	
+	public InArgumentDefinition(ActivityWithResultDefinition activityWithResult) {
+		this.activityWithResult = activityWithResult;
+	}
+	
 	public InArgument toArgument(ActivityDefinition parent, DefinitionValidator validator) {
 		if (this.function != null)
 			return new InArgument(this.function.toFunction(parent, validator));
-
+		
 		if (this.variable != null)
 			return new InArgument(this.variable.toVariable(parent, validator));
-
+		
+		if (this.activityWithResult != null)
+			return new InArgument(this.activityWithResult.toActivity(parent, validator));
+		
 		return new InArgument(this.constValue);
 	}
 }
